68fb8f463e5e3a2d4e7970263e09c19c0d283061
shelleyyyyy <shelleywr23@mail.vmi.edu>
2023-04-02 12:43:28 -0400

    test

--- battery/miniverse.py
+++ battery/miniverse.py
@@ -31,7 +31,7 @@ def make_net():
         try:
             if x['type'] == "host":
                 host = net.addHost(x['name'], defaultRoute=default_route)
-                host.popen("bash /./h1_squid.sh")
+                host.popen("bash ./h1_squid.sh")
                 # host.popen("python3 /device.py " + x['device'] + " " + x['topic']) 
                 nodes.append(host)
             elif x['type'] == "switch":
